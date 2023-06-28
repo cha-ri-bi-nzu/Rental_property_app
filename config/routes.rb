aa330792215be4ca2e3554578d7d7de3b40cf500
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-28 15:26:42 +0900

    controller・viewファイス作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'properties/index'
+  get 'properties/new'
+  get 'properties/create'
+  get 'properties/show'
+  get 'properties/edit'
+  get 'properties/update'
+  get 'properties/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
