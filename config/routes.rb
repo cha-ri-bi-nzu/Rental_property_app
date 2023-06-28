05f19ffedc949b27bbcce5f0555d4e919e56bbf1
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-28 15:42:45 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'properties/index'
-  get 'properties/new'
-  get 'properties/create'
-  get 'properties/show'
-  get 'properties/edit'
-  get 'properties/update'
-  get 'properties/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "properties#index"
+  resources properties 
 end
