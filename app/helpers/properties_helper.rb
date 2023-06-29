d8b09e78e191bb98a54e5b9e23184d5a1b6b270f
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-29 22:56:59 +0900

    helperのメソッド修正

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,6 +1,6 @@
 module PropertiesHelper
   def choose_url
-    if edit_or_new
+    if edit_or_new?
       property_path
     else
       properties_path
@@ -11,7 +11,7 @@ module PropertiesHelper
     if action_name == 'edit' || action_name == 'update'
       true
     elsif action_name == 'new' || action_name == 'create'
-      fail
+      false
     end
   end
 end
